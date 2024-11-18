aa54f0b2cba57f8fb19dfc2e7c67a22a652b3d89
Andy Wang <andywang@Andys-Laptop-2.local>
2024-11-17 17:20:11 -0800

    removed logical connectives

--- src/test/java/verifier/equality/Equality.dfy
+++ src/test/java/verifier/equality/Equality.dfy
@@ -3,8 +3,12 @@ method CheckIfEqual(x: int, y: int) returns (equal: int)
   ensures (x <= y) && (y <= x) ==> equal == 1
   ensures !(x <= y) || !(y <= x) ==> equal == 0
 {
-  if (x <= y) && (y <= x) {
-    equal := 1;
+  if (x <= y) {
+    if(y <= x) {
+      equal := 1;
+    } else {
+      equal := 0; 
+    }
   } else {
     equal := 0;
   }
--- src/test/java/verifier/equality/VerifierEqualityTest.java
+++ src/test/java/verifier/equality/VerifierEqualityTest.java
@@ -15,39 +15,49 @@ public class VerifierEqualityTest {
 	private Statement program;
 	
 	private String expectedSerializedProgram(){
-	  String expected = "if ( x <= y ) AND ( y <= x )\n";
+	  String expected = "if x <= y\n";
     expected += "then\n";
-    expected += "  equal := 1\n";
+    expected += "  if y <= x\n";
+    expected += "  then\n";
+    expected += "    equal := 1\n";
+    expected += "  else\n";
+    expected += "    equal := 0\n";
     expected += "else\n";
     expected += "  equal := 0";
-	  return expected;
+    return expected;
 	} 
 	
 	private Statement createProgram(){
 
-    // If Condition 
+    // -- Outer If Statement -- 
+    // Outer Condition 
     Expression x1 = new VariableExpression("x");
     Expression y1 = new VariableExpression("y");
-    Condition c1 = new BinaryCondition(ConditionType.LE, x1, y1);
+    Condition outerCondition = new BinaryCondition(ConditionType.LE, x1, y1);
 
-    Expression y2 = new VariableExpression("y");
-    Expression x2 = new VariableExpression("x");
-    Condition c2 = new BinaryCondition(ConditionType.LE, y2, x2);
-    
-    Condition condition = new BinaryConnective(ConnectiveType.AND, c1, c2);
-
-    // Then Block
-    VariableExpression equal1 = new VariableExpression("equal");
-    IntegerExpression one = new IntegerExpression(1);
-    Assignment thenBlock = new Assignment(equal1, one);
-
-    // Else Block 
+    // -- Outer Then / Inner If Statement --
+    // innerCondition
+    VariableExpression y2 = new VariableExpression("y");
+    VariableExpression x2 = new VariableExpression("x");
+    Condition innerCondition = new BinaryCondition(ConditionType.LE, y2, x2);
+    // innerThenBlock
     VariableExpression equal2 = new VariableExpression("equal");
-    IntegerExpression zero = new IntegerExpression(0);
-    Assignment elseBlock = new Assignment(equal2, zero);
+    IntegerExpression one1 = new IntegerExpression(1);
+    Assignment innerThenBlock = new Assignment(equal2, one1);
+    // innerElseBlock
+    VariableExpression equal3 = new VariableExpression("equal");
+    IntegerExpression zero2 = new IntegerExpression(0);
+    Assignment innerElseBlock = new Assignment(equal3, zero2);
+    
+    Statement innerIfStatement = new If(innerCondition, innerThenBlock, innerElseBlock);
 
-    Statement program = new If(condition, thenBlock, elseBlock);
-		
+    // Outer Else 
+    VariableExpression equal1 = new VariableExpression("equal");
+    IntegerExpression zero1 = new IntegerExpression(0);
+    Assignment outerElseBlock = new Assignment(equal1, zero1);
+
+    Statement program = new If(outerCondition, innerIfStatement, outerElseBlock);
+
 		// Verify program serialization
 		program.accept(visitor);
 		String result = visitor.result;
